c6dccef0fb222985070ae93c98f161cbd86e88fa
Nouman Javed <noumanmalik960@gmail.com>
2020-03-26 22:43:39 +0500

    added temp.py file

---
+++ temp.py
@@ -0,0 +1,17 @@
+# Inheritance in python
+
+# class Person():
+#     def __init__(self, n, fn):
+#         self.name = n
+#         self.fname = fn
+
+#     def showName(self):
+#         print(self.name.title(), self.fname.title())
+
+
+# class Student(Person):
+#     def __init__(self, n, fn):
+
+
+# x = Student("nouman", "javed")
+# x.showName()
